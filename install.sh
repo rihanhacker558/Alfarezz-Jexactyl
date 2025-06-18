f83fd83e9cf31735ebd3b0545af2eaf66943b4cc
rihanhacker558 <rihanhacker6@gmail.com>
2025-06-18 23:27:25 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install git -y && sudo apt install curl -y
+sudo apt install ffmpeg -y && sudo apt install webp -y && sudo apt install ffmpeg -y && sudo apt install imagemagick -y
+sudo apt install zip -y && sudo apt install unzip -y
+sudo apt-get install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install 18.19.1
+nvm use 18.19.1
+npm i pm2 -g
+curl -o- -L https://yarnpkg.com/install.sh | bash
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo apt install yarn -y
+sudo apt install neofetch -y
